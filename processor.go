819e3f5f33ce0e5828dc68d5985f4334ddc00111
Masayoshi Mizutani <mizutani@cookpad.com>
2019-09-13 15:20:51 +0900

    emitterMode is set automatically in newPacketProcessor

--- processor.go
+++ processor.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type packetProcessor struct {
 	Argument packetProcessorArgument
 	emitter  recordEmitter
@@ -10,7 +12,32 @@ type packetProcessorArgument struct {
 	EmitterArgs emitterArgument
 }
 
+type emitterModeKey struct {
+	Emitter string
+	Format  string
+	Target  string
+}
+
+var emitterModeMap = map[emitterModeKey]string{
+	{Emitter: "fs", Format: "pcap", Target: "packet"}: "stream",
+	{Emitter: "fs", Format: "json", Target: "packet"}: "stream",
+}
+
 func newPacketProcessor(args packetProcessorArgument) (*packetProcessor, error) {
+	// Choose emitter mode
+	modeKey := emitterModeKey{
+		Emitter: args.EmitterArgs.Key.Name,
+		Format:  args.DumperKey.Format,
+		Target:  args.DumperKey.Target,
+	}
+
+	emitterMode, ok := emitterModeMap[modeKey]
+	if !ok {
+		return nil, fmt.Errorf("The settings for emitter and dumper are not allowed: %v", modeKey)
+	}
+	args.EmitterArgs.Key.Mode = emitterMode
+
+	// construct dumper and emitter
 	dumper, err := getDumper(args.DumperKey)
 	if err != nil {
 		return nil, err
